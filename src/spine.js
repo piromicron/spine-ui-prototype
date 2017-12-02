649e3cbf6d18346f9a039c956e9e3b60b144028d
piromicron <piromicron@gmail.com>
2017-12-02 08:03:53 +0300

    structure more

--- src/spine.js
+++ src/spine.js
@@ -9,18 +9,19 @@ function scrollToStart() {
 	}
 }
 
-var points = {
 
-	"test": {
-		x: 0,
-		y: 0,
-		place: "center"
-	},
-};
 
 function loadPoints() {
+	let vertebraLabels = ["C5","C6","C7","Th1","Th2","Th3","Th4","Th5","Th6","Th7"];
 
-	var vertebraLabels = ["C5","C6","C7","Th1","Th2","Th3","Th4","Th5","Th6","Th7"];
+	let points = {
+
+		"test": {
+			x: 0,
+			y: 0,
+			place: "center"
+		},
+	};
 
 	const minY=400;
 	const maxY=1700;
@@ -34,41 +35,48 @@ function loadPoints() {
 		points[vertebraLabels[i]+"r"] = { "y": minY+dY*i, "x": cX-dX, place: "right" };
 		points[vertebraLabels[i]+"l"] = { "y": minY+dY*i, "x": cX+dX, place: "left" };
 	}
+	return points;
 }
 
-function markPoints() {
+function createMark(pointid, point) {
 	const centerOfPointImage = (40*90/150);//+1; //TODO: why?!        
+
+	let left = point.x-centerOfPointImage;
+	let top = point.y-centerOfPointImage;
+	let style = "left: "+left+"; top: "+top+"; position: absolute;";
+	console.log("Point #" + pointid );
+	let svg = $("svg#vertebra-point-template").clone()
+		.attr( "id", pointid)
+		.addClass("vertebra-point set " + point.place)
+		;
+	let div = $(svg).wrap("<div id=\"div_"+pointid+"\" style=\""+style+"\"></div>").parent(); 
+
+	let labelElemeent = $(svg).find("text").get(0);
+	let labelClass = "svg-label-"+ point.place;
+	$(svg).find("text").each(function () { $(this).html(pointid); $(this).addClass(labelClass); });                     
+	return div;	
+}
+
+function set(pointid, point) {
+	let element = createMark(pointid, point);
+	$("div#image").append(element);
+	element.draggable();    
+}
+
+
+function markPoints(points) {
 	
 	for(var pointid in points) {
 		let point = points[pointid];
 		console.log("Point #" + pointid + ": { x: "+ point.x+", y: "+ point.y + ", place: "+ point.place );
-			
-		function set( pointid, point ) {
-			let left = point.x-centerOfPointImage;
-			let top = point.y-centerOfPointImage;
-			let style = "left: "+left+"; top: "+top+"; position: absolute;";
-			console.log("Point #" + pointid );
-			let svg = $("svg#vertebra-point-template").clone()
-				.attr( "id", pointid)
-				.addClass("vertebra-point set " + point.place)
-				;
-			let div = $(svg).wrap("<div id=\"div_"+pointid+"\" style=\""+style+"\"></div>").parent(); 
-
-			let labelElemeent = $(svg).find("text").get(0);
-			let labelClass = "svg-label-"+ point.place;
-			$(svg).find("text").each(function () { $(this).html(pointid); $(this).addClass(labelClass); });                     
-			$("div#image").append(div);
-			div.draggable();    
-			};
-			
 		set(pointid,point);
 	}
 }
 
 function doit() {
 	scrollToStart();
-	loadPoints();
-	markPoints();
+	let points = loadPoints();
+	markPoints(points);
 }
 
 function loadImage() {
