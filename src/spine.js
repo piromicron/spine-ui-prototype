57a999f62e3b9d4a264a476f13fc135d90eefce0
piromicron <piromicron@gmail.com>
2017-12-13 00:39:18 +0300

    Load image link to local file selector instead of hardcoded image url .

--- src/spine.js
+++ src/spine.js
@@ -5,6 +5,9 @@
 
 
 class Editor {
+	constructor() {
+		Editor.installLoadFileHandler();
+	}
 	open(url) {
 		this.loadImage(url);
 	}
@@ -22,6 +25,12 @@ class Editor {
 		document.body.appendChild(marker.element);
 		marker.markPoints(points);
 		this.scrollToStart();
+
+		//TODO: We got this url from uploaded file don't need it anymore, so revoke blob 
+		//In fact we may need this image file later (i.e. for saving with poins) so let's keep it at least for now
+		//if( url.startsWith("blob:") ) { 
+		//	window.URL.revokeObjectURL(this.src); // Revoke blob file
+		//}
 	}
 	scrollToStart() {
 		let imageWidth = this.$image().width();
@@ -35,7 +44,35 @@ class Editor {
 	}
 	$image() {
 		return $("img#theimage");
+
 	}
+	static installLoadFileHandler() {
+		window.URL = window.URL || window.webkitURL;
+
+		var fileSelect = document.getElementById("fileSelect"),
+		    fileElem = document.getElementById("fileElem"),
+		    fileList = document.getElementById("fileList");
+
+		fileSelect.addEventListener("click", 
+			function (e) {
+				if (fileElem) {
+					fileElem.click();
+				}
+				e.preventDefault(); // prevent navigation to "#"
+			}, false
+		);
+	}
+	static handleFiles(files) {
+		if (!files.length) {
+			//TODO fileList.innerHTML = "<p>No files selected!</p>";
+		} 
+		else {
+			//TODO fileList.innerHTML = "";
+ 			const imageURL = window.URL.createObjectURL(files[0]);
+			Editor_theEditor.open(imageURL);
+		}
+	}
+
 
 	static getEditor() {
 		return Editor_theEditor; 
@@ -43,7 +80,4 @@ class Editor {
 }
 
 var Editor_theEditor = new Editor();
-
-
-const imageURL = "IMG-0001-00001.jpg";
-Editor_theEditor.open(imageURL);
+//Editor.installLoadFileHandler();
